51f010213e2affba20c23f7c8bc007f62ca36a5c
Sunil <sunil016@yahoo.com>
2023-10-01 11:39:53 +0530

    add test and stack for s3-event-notification

--- lambda-sqs/test/lambda-sqs-event.test.ts
+++ lambda-sqs/test/lambda-sqs-event.test.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as LambdaSqsEvent from '../lib/lambda-sqs-event-stack';
 
-test('SQS Queue Created', () => {
+test('Lambda Queue Created', () => {
     const app = new cdk.App();
     const stack = new LambdaSqsEvent.LambdaSqsEventStack(app, 'MyTestStack');
     const template = Template.fromStack(stack);
--- s3-sqs-event-notification/lib/s3-sqs-event-notification-stack.ts
+++ s3-sqs-event-notification/lib/s3-sqs-event-notification-stack.ts
@@ -1,16 +1,52 @@
 import * as cdk from 'aws-cdk-lib';
+import { IBucket } from 'aws-cdk-lib/aws-s3';
+import { SqsDestination } from 'aws-cdk-lib/aws-s3-notifications';
+import { IQueue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+/**
+ * Create Stack, to create S3 and Queue with Event Notification
+ *
+ * @export
+ * @class S3SqsEventNotificationStack
+ * @extends {cdk.Stack}
+ */
 export class S3SqsEventNotificationStack extends cdk.Stack {
+
+  // Prefix to S3 Bucket, to be used to trigger s3 event.
+  readonly prefix_s3_file = 'aws-sample'
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const s3Bucket = this.createS3Bucket()
+    const sqsQueue = this.createEventQueue()
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3SqsEventNotificationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    s3Bucket.addObjectCreatedNotification(new SqsDestination(sqsQueue), {
+      prefix: this.prefix_s3_file
+    });
+  }
+
+  /**
+   * Create S3 Bucket
+   * @return {*}  {IBucket}
+   * @memberof S3SqsEventNotificationStack
+   */
+  createS3Bucket() : IBucket{
+    return new cdk.aws_s3.Bucket(this, 's3Bucket', {
+      encryption : cdk.aws_s3.BucketEncryption.S3_MANAGED
+    })
+  }
+
+  /**
+   * Creating Queue, which will be use to consume event from S3
+   *
+   * @return {*}  {IQueue}
+   * @memberof S3SqsEventNotificationStack
+   */
+  createEventQueue(): IQueue {
+    return new cdk.aws_sqs.Queue(this, 'SqsQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
   }
 }
